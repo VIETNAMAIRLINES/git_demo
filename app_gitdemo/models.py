8f7acdc531c8adadd749c445338015eb4b96f613
Hieu Nguyen Trung <hieunt.nro@gmail.com>
2013-12-10 16:20:31 +0700

    add TODO

--- app_gitdemo/models.py
+++ app_gitdemo/models.py
@@ -3,3 +3,5 @@ from django.db import models
 class AWBS(models.Model): # model chua KHONG VAN DON
     number = models.CharField(max_length=15)        
     dateCreated = models.DateField()
+        
+# TODO: add new model
